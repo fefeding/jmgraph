b5f8a01be756b7ba92d69d92d7ad9db954f34f04
fefeding <fefeding@tencent.com>
2020-06-08 11:47:59 +0800

    增加构建core

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -11,6 +11,9 @@ const browserify = require('browserify');
 const buffer = require('vinyl-buffer');
 const source = require('vinyl-source-stream');
 const gulp = require('gulp');
+const rollup = require('rollup');
+const rollupugify = require('rollup-plugin-uglify');
+const rollupbabel = require('rollup-plugin-babel');
 const cleanimport = require('gulp-clean-import');
 const gulpJsdoc2md = require('gulp-jsdoc-to-markdown')
 
@@ -66,23 +69,50 @@ gulp.task('build-js-es6', function () {
 
 //编译core成es5版本
 gulp.task('build-js-core', function () {
-    return browserify({
+    /*return browserify({
         entries: [
             '../src/core/jmGraph.js'
         ]
-      }).plugin(standalonify, {
-          name: 'jmGraph'   //转为umd规范
-      })
+      })//.plugin(standalonify, {
+        //  name: 'jmGraph'   //转为umd规范
+      //})
     .transform("babelify", {
-        presets: ['@babel/preset-env']
+        presets: ['@babel/preset-env'],
+        //plugins: ['transform-runtime'] 
     })  //使用babel转换es6代码
     .bundle()  //合并打包
+    .on('error', function(err) {
+        console.log(err);
+    })
     .pipe(source('jmgraph.core.js'))
     .pipe(buffer())
     .pipe(gulp.dest('../dist'))
     .pipe(rename('jmgraph.core.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('../dist'));
+    .pipe(gulp.dest('../dist'));*/
+
+    return rollup.rollup({
+        input: '../src/core/jmGraph.js',
+        plugins: [
+            rollupbabel({
+                babelrc: false,                        //不设置.babelrc文件;
+                exclude: 'node_modules/**',            //排除node_modules文件夹;
+                presets: [['@babel/env', { modules: false }]],
+                //plugins: ['transform-class-properties']//转换静态类属性以及属性声明的属性初始化语法
+            }),
+            rollupugify.uglify({
+                sourcemap: true
+            })
+        ]
+      }).then(bundle => {
+        
+        return bundle.write({
+          file: '../dist/jmgraph.core.min.js',
+          format: 'umd',
+          name: 'jmGraph',
+          sourcemap: true
+        });
+      });
 });
 
 //编译成es5版本
